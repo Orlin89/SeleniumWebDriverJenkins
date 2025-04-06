e598e68d87886bb1b4d8ed6105e2a6f1b8f55475
Orlin89 <149891759+Orlin89@users.noreply.github.com>
2025-04-06 17:22:14 +0300

    Configure chrome options

--- TestProject2/DropDown.cs
+++ TestProject2/DropDown.cs
@@ -15,8 +15,17 @@ namespace TestProject3
         [SetUp]
         public void SetUp()
         {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("headless");
+            options.AddArguments("no-sandbox");
+            options.AddArguments("disable-dev-shm-usage");
+            options.AddArguments("disable-gpu");
+
+            string userDataDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(userDataDir);
+
             // Create object of ChromeDriver
-            driver = new ChromeDriver();
+            driver = new ChromeDriver(userDataDir);
 
             // Add implicit wait
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
